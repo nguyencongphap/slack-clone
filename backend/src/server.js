75bdccb21e0d836ccad76c513a40a010d23a6bbc
PHAP_LAPTOP\ncpha <nguyencongphap@yahoo.com>
2025-09-29 19:19:47 -0700

    Connected to DB and added env vars of other tools

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { ENV } from "./config/env.js"; // add .js because we're importing local files
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
@@ -7,6 +8,7 @@ app.get("/", (req, res) => {
   res.send("Hello World! 123");
 });
 
-console.log("mongouri: ", ENV.MONGO_URI);
-
-app.listen(ENV.PORT, () => console.log("Server started on port:", ENV.PORT));
+app.listen(ENV.PORT, () => {
+  console.log("Server started on port:", ENV.PORT);
+  connectDB();
+});
